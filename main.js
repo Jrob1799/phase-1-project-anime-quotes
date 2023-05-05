c3200e7e2d4712c5d4e02b3af87f38b4d580cb34
Jacob <jacobwroberts33@gmail.com>
2023-05-05 01:47:41 -0400

    add quote button and make it fetch random quote

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-console.log("IT'S ALIVE")
+function randomQuote(event) {
+    fetch("https://animechan.vercel.app/api/random")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+    return data
+    })
+    .then((data) => {
+        let quote = data.quote
+        console.log(quote)
+        const quoteResult = document.getElementById("quoteResult")
+    })
+    
+    event.preventDefault()
+}
 
-fetch("https://animechan.vercel.app/api/random")
-.then((response) => response.json())
-.then((quote) => console.log(quote));
+
+const quoteButton = document.getElementById("quoteButton")
+    quoteButton.addEventListener('click', randomQuote)
+
+
