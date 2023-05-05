f9dfd662b05ef090f8147ae08ad8ed1f6086ce6a
Jacob <jacobwroberts33@gmail.com>
2023-05-05 02:19:13 -0400

    remove console.log throughout main.js and clean up code

--- main.js
+++ main.js
@@ -1,25 +1,16 @@
 function randomQuote(event) {
     fetch("https://animechan.vercel.app/api/random")
     .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
-    return data
-    })
+    .then((data) => {return data})
     .then((data) => {
         let quote = data.quote
         let anime = data.anime
         let character = data.character
-        console.log(quote)
-        console.log(anime)
-        console.log(character)
         let quoteLine = document.getElementById("animeQuote").textContent = quote;
         let charLine = document.getElementById("animeCharacter").textContent = character;
         let titleLine = document.getElementById("animeTitle").textContent = anime;
     })
-    
-    event.preventDefault()
 }
-
 
 const quoteButton = document.getElementById("quoteButton")
     quoteButton.addEventListener('click', randomQuote)
