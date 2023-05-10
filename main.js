92ae1cae83a316123c1efa7a1dc14496577587b3
Jacob <jacobwroberts33@gmail.com>
2023-05-09 23:23:40 -0400

    change headings to labels

--- main.js
+++ main.js
@@ -35,11 +35,18 @@ function ListHandler(Array) {
 }
 
 // returns a quote by anime Title
-function searchQuote(title) {
-    fetch(`https://animechan.vercel.app/api/random/anime?title=${title}`)
-          .then(r => r.json())
-          .then((data) => console.log(data));
-}
+// function searchQuote(title) {
+//     fetch(`https://animechan.vercel.app/api/random/anime?title=${title}`)
+//           .then(r => r.json())
+//           .then((data) => () {
+//             let quote = data.quote
+//             let anime = data.anime
+//             let character = data.character
+//             let quoteLine = document.getElementById("animeQuote").textContent = quote;
+//             let charLine = document.getElementById("animeCharacter").textContent = character;
+//             let titleLine = document.getElementById("animeTitle").textContent = anime;
+//           });
+// }
 
 // uses searchQuote to return a quote from the anime selected in the dropdown menu
 function animeSelector(title) {
